06fe3690811bc0f3f6aa5753d2c405136bb2af86
Samuel Gratzl <sam@sgratzl.com>
2019-11-15 16:15:13 -0500

    start with custom widget

--- django/joist/forms.py
+++ django/joist/forms.py
@@ -1,9 +1,13 @@
 from django.forms import ModelForm
 
 from .models import Blob
+from core.widgets import S3FileInput
 
 
 class BlobForm(ModelForm):
     class Meta:
         model = Blob
         fields = ['creator', 'created', 'resource']
+        widgets = {
+            'resource': S3FileInput(),
+        }
