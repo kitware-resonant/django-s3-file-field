06c0d3d03370f8829d7f22a8ba0992d4f8da974f
Samuel Gratzl <sam@sgratzl.com>
2019-11-15 16:17:55 -0500

    use custom widget also in admin page

--- django/joist/admin.py
+++ django/joist/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 from .models import Blob
+from .forms import BlobForm
 
 
 @admin.register(Blob)
 class BlobAdmin(admin.ModelAdmin):
-    pass
+    form = BlobForm
