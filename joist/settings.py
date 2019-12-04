0635967d371c21f2aba914f55dd95b2d54478a41
Samuel Gratzl <sam@sgratzl.com>
2019-12-04 16:21:17 -0500

    add fallback logic

--- joist/settings.py
+++ joist/settings.py
@@ -1,17 +1,11 @@
+import logging
 from typing import Any
 
 from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
 
 
 def _s(key: str, default: Any = None):
     return getattr(settings, key, default)
-
-
-def _r(key: str):
-    if not hasattr(settings, key):
-        raise ImproperlyConfigured(f'joist requires the `{key}` setting')
-    return getattr(settings, key)
 
 
 # configuration from django-storage for S3
@@ -19,9 +13,18 @@ AWS_ACCESS_KEY_ID = _s('AWS_S3_ACCESS_KEY_ID', _s('AWS_ACCESS_KEY_ID'))
 AWS_SECRET_ACCESS_KEY = _s('AWS_S3_SECRET_ACCESS_KEY', _s('AWS_SECRET_ACCESS_KEY'))
 AWS_REGION = _s('AWS_S3_REGION_NAME', _s('AWS_REGION'))
 
-AWS_STORAGE_BUCKET_NAME = _r('AWS_STORAGE_BUCKET_NAME')
+AWS_STORAGE_BUCKET_NAME = _s('AWS_STORAGE_BUCKET_NAME')
 
-JOIST_UPLOAD_STS_ARN = _r('JOIST_UPLOAD_STS_ARN')
+JOIST_UPLOAD_STS_ARN = _s('JOIST_UPLOAD_STS_ARN')
+
+USE_FALLBACK = AWS_STORAGE_BUCKET_NAME is None or JOIST_UPLOAD_STS_ARN is None
+
+if USE_FALLBACK:
+    logging.getLogger('joist').warning(
+        'joist is not configured properly missing '
+        'AWS_STORAGE_BUCKET_NAME or JOIST_UPLOAD_STS_ARN, will fall back to default upload'
+    )
+
 
 # in seconds 60 * 60 * 12 = 12h
 JOIST_UPLOAD_DURATION = _s('JOIST_UPLOAD_DURATION', 60 * 60 * 12)
--- joist/urls.py
+++ joist/urls.py
@@ -2,12 +2,14 @@ from typing import Any, List
 
 from django.urls import path
 
+from .settings import USE_FALLBACK
 from .views import upload_finalize, upload_prepare
 
-urlpatterns = [
-    path('upload-prepare/', upload_prepare),
-    path('upload-finalize/', upload_finalize),
-]
+urlpatterns = (
+    []
+    if USE_FALLBACK
+    else [path('upload-prepare/', upload_prepare), path('upload-finalize/', upload_finalize)]
+)
 
 
 def add_url_patterns(patterns: List[Any], prefix='api/joist'):
