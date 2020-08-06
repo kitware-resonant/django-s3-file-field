31fed55f5d209f6315c918d408dc955a1f7e73e8
Daniel Chiquito <daniel.chiquito@kitware.com>
2020-08-06 16:45:54 -0400

    Fix bad imports which broke everything

--- s3_file_field/urls.py
+++ s3_file_field/urls.py
@@ -2,14 +2,13 @@ from typing import List
 
 from django.urls import path
 
-from .configuration import StorageProvider
-from .settings import _S3FF_STORAGE_PROVIDER
+from .constants import S3FF_STORAGE_PROVIDER, StorageProvider
 from .views import upload_finalize, upload_prepare
 
 app_name = 's3_file_field'
 urlpatterns: List = []
 
-if _S3FF_STORAGE_PROVIDER != StorageProvider.UNSUPPORTED:
+if S3FF_STORAGE_PROVIDER != StorageProvider.UNSUPPORTED:
     urlpatterns += [
         path('upload-prepare/', upload_prepare, name='upload-prepare'),
         path('upload-finalize/', upload_finalize, name='upload-finalize'),
