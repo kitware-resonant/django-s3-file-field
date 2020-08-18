923aa919038712b6cec788b6d1c39c196655c836
Daniel Chiquito <daniel.chiquito@kitware.com>
2020-08-18 17:27:24 -0400

    Add multipart upload test

--- example/tests/conftest.py
+++ example/tests/conftest.py
@@ -1,7 +1,14 @@
 import pytest
 from rest_framework.test import APIClient
 
+from s3_file_field.boto import client_factory
+
 
 @pytest.fixture
 def api_client() -> APIClient:
     return APIClient()
+
+
+@pytest.fixture
+def boto_client():
+    return client_factory('s3')
