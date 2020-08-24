33e01911e3c83446498f07f89399c6b783ad23d5
Daniel Chiquito <daniel.chiquito@kitware.com>
2020-08-24 11:59:12 -0400

    Add more multipart upload tests
    Also add a small library `fuzzy` for fuzzy matching of URLs and UUIDs.

---
+++ example/tests/fuzzy.py
@@ -0,0 +1,22 @@
+import re
+
+
+class Re:
+    def __init__(self, pattern):
+        if isinstance(pattern, type(re.compile(''))):
+            self.pattern = pattern
+        else:
+            self.pattern = re.compile(pattern)
+
+    def __eq__(self, other):
+        return self.pattern.fullmatch(other) is not None
+
+    def __str__(self):
+        return self.pattern.pattern
+
+    def __repr__(self):
+        return repr(self.pattern.pattern)
+
+
+URL_RE = Re(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+UUID_RE = Re(r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}')
--- example/tests/test_multipart_upload.py
+++ example/tests/test_multipart_upload.py
@@ -1,29 +1,109 @@
 import json
 
+from fuzzy import Re, URL_RE, UUID_RE
+import pytest
 import requests
 
 
-def test_multipart_upload(api_client, boto_client, bucket):
-    # Initialize the multipart upload
+def test_prepare(api_client, boto_client, bucket):
+    # contents has length 10B
+    contents = b'ten bytes\n'
+
     resp = api_client.post(
         '/api/joist/multipart-upload-prepare/',
-        json.dumps({'name': 'test.txt', 'parts': 1}),
+        json.dumps({'name': 'test.txt', 'content_length': len(contents)}),
         content_type='application/json',
     )
     assert resp.status_code == 200
+    assert resp.json() == {
+        'parts': [{'url': URL_RE, 'part_number': 0, 'content_length': 10}],
+        'key': Re(r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/test.txt'),
+        'upload_id': UUID_RE,
+    }
 
+
+def test_prepare_two_parts(api_client, boto_client, bucket):
+    # contents has length 10MB
+    contents = b'ten bytes\n' * 1_000_000
+
+    resp = api_client.post(
+        '/api/joist/multipart-upload-prepare/',
+        json.dumps(
+            {'name': 'test.txt', 'content_length': len(contents), 'max_part_length': 5_000_000}
+        ),
+        content_type='application/json',
+    )
+    assert resp.status_code == 200
+    assert resp.json() == {
+        'parts': [
+            {'url': URL_RE, 'part_number': 0, 'content_length': 5_000_000},
+            {'url': URL_RE, 'part_number': 1, 'content_length': 5_000_000},
+        ],
+        'key': Re(r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/test.txt'),
+        'upload_id': UUID_RE,
+    }
+
+
+def test_prepare_three_parts(api_client, boto_client, bucket):
+    # contents has length 15MB
+    contents = b'ten bytes\n' * 1_500_000
+
+    resp = api_client.post(
+        '/api/joist/multipart-upload-prepare/',
+        json.dumps(
+            {'name': 'test.txt', 'content_length': len(contents), 'max_part_length': 6_000_000}
+        ),
+        content_type='application/json',
+    )
+    assert resp.status_code == 200
+    assert resp.json() == {
+        'parts': [
+            {'url': URL_RE, 'part_number': 0, 'content_length': 6_000_000},
+            {'url': URL_RE, 'part_number': 1, 'content_length': 6_000_000},
+            {'url': URL_RE, 'part_number': 2, 'content_length': 3_000_000},
+        ],
+        'key': Re(r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/test.txt'),
+        'upload_id': UUID_RE,
+    }
+
+
+def test_prepare_small_max_part_length_fails(api_client, boto_client, bucket):
+    contents = b'File contents!\n'
+
+    with pytest.raises(ValueError) as excinfo:
+        api_client.post(
+            '/api/joist/multipart-upload-prepare/',
+            json.dumps(
+                {'name': 'test.txt', 'content_length': len(contents), 'max_part_length': 4_999_999}
+            ),
+            content_type='application/json',
+        )
+    assert 'max_part_length must be greater than 5MB' in str(excinfo.value)
+
+
+def test_finalize(api_client, boto_client, bucket):
+    # contents has length 10B
+    contents = b'ten bytes\n'
+
+    # Initialize the multipart upload
+    resp = api_client.post(
+        '/api/joist/multipart-upload-prepare/',
+        json.dumps({'name': 'test.txt', 'content_length': len(contents)}),
+        content_type='application/json',
+    )
+    assert resp.status_code == 200
     resp_json = resp.json()
+    assert resp_json == {
+        'parts': [{'url': URL_RE, 'part_number': 0, 'content_length': 10}],
+        'key': Re(r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/test.txt'),
+        'upload_id': UUID_RE,
+    }
     key = resp_json['key']
     upload_id = resp_json['upload_id']
     parts = resp_json['parts']
 
-    assert len(parts) == 1
-    assert parts[0]['part_number'] == 0
-    upload_url = parts[0]['url']
-
     # Perform the upload
-    contents = b'File contents!\n'
-    resp = requests.put(upload_url, data=contents)
+    resp = requests.put(parts[0]['url'], data=contents)
     assert resp.status_code == 200
     etag = resp.headers['ETag']
 
@@ -32,6 +112,60 @@ def test_multipart_upload(api_client, boto_client, bucket):
         '/api/joist/multipart-upload-finalize/',
         json.dumps(
             {'key': key, 'upload_id': upload_id, 'parts': [{'part_number': 0, 'etag': etag}]}
+        ),
+        content_type='application/json',
+    )
+    assert resp.status_code == 201
+
+    # Verify the object is present in the store
+    resp = boto_client.get_object(Bucket=bucket, Key=key)
+    assert resp['Body'].read() == contents
+
+
+def test_finalize_two_parts(api_client, boto_client, bucket):
+    # contents has length 12MB
+    # Minio seems to have a slightly higher cap than 5MB, so we will use a max part of 6MB
+    contents = b'ten bytes\n' * 1_200_000
+
+    # Initialize the multipart upload
+    resp = api_client.post(
+        '/api/joist/multipart-upload-prepare/',
+        json.dumps(
+            {'name': 'test.txt', 'content_length': len(contents), 'max_part_length': 6_000_000}
+        ),
+        content_type='application/json',
+    )
+    assert resp.status_code == 200
+    resp_json = resp.json()
+    assert resp_json == {
+        'parts': [
+            {'url': URL_RE, 'part_number': 0, 'content_length': 6_000_000},
+            {'url': URL_RE, 'part_number': 1, 'content_length': 6_000_000},
+        ],
+        'key': Re(r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/test.txt'),
+        'upload_id': UUID_RE,
+    }
+    key = resp_json['key']
+    upload_id = resp_json['upload_id']
+    parts = resp_json['parts']
+
+    # Perform the upload
+    resp = requests.put(parts[0]['url'], data=contents[:6_000_000])
+    assert resp.status_code == 200
+    etag0 = resp.headers['ETag']
+    resp = requests.put(parts[1]['url'], data=contents[6_000_000:])
+    assert resp.status_code == 200
+    etag1 = resp.headers['ETag']
+
+    # Finalize the upload
+    resp = api_client.post(
+        '/api/joist/multipart-upload-finalize/',
+        json.dumps(
+            {
+                'key': key,
+                'upload_id': upload_id,
+                'parts': [{'part_number': 0, 'etag': etag0}, {'part_number': 1, 'etag': etag1}],
+            }
         ),
         content_type='application/json',
     )
