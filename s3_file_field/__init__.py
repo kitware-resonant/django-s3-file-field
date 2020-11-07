06323e77baa8a2c84fc2544ad6ae95e5c1ce94ca
Kushagra Garg <57121588+kushagra219@users.noreply.github.com>
2020-11-07 21:17:52 +0530

    Update __init__.py

--- s3_file_field/__init__.py
+++ s3_file_field/__init__.py
@@ -5,12 +5,3 @@ from .fields import S3FileField  # noqa: F401
 
 default_app_config = 's3_file_field.apps.S3FileFieldConfig'
 
-try:
-    from importlib import metadata
-except ImportError:
-    # Running on pre-3.8 Python; use importlib-metadata package
-    import importlib_metadata as metadata
-
-assert metadata.version('django-s3-file-field') == '0.0.14'
-
-__version__ = metadata.version('django-s3-file-field')
