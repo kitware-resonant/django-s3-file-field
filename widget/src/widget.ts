650329626f73015fbc7d4e25baef11f97024703b
Brian Helba <brian.helba@kitware.com>
2021-01-18 21:20:45 -0500

    Address airbnb lint findings

--- widget/src/widget.ts
+++ widget/src/widget.ts
@@ -1,19 +1,19 @@
-import "./style.scss";
-import S3FileInput from "./S3FileInput";
+import './style.scss';
+import S3FileInput from './S3FileInput';
 
 /**
  * @internal
  */
-export function autoInit(): void {
+function autoInit(): void {
   // auto init
   const wrappers = Array.from(
-    document.querySelectorAll<HTMLInputElement>("input[data-s3fileinput]")
+    document.querySelectorAll<HTMLInputElement>('input[data-s3fileinput]'),
   );
-  wrappers.forEach(w => new S3FileInput(w));
+  wrappers.forEach((w) => new S3FileInput(w));
 }
 
-if (document.readyState != "loading") {
+if (document.readyState !== 'loading') {
   autoInit();
 } else {
-  document.addEventListener("DOMContentLoaded", autoInit);
+  document.addEventListener('DOMContentLoaded', autoInit);
 }
