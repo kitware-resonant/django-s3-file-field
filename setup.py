5fdb5b67db932d52f2b7e7335a9755a94dcd6d70
Brian Helba <brian.helba@kitware.com>
2021-01-19 08:23:41 -0500

    Use PEP-518 to build the package

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-import os
-
 from setuptools import find_packages, setup
 from setuptools.command.build_py import build_py
 from setuptools.command.egg_info import egg_info
@@ -9,31 +6,12 @@ from setuptools.command.sdist import sdist
 from build_js import NPM, js_prerelease
 
 
-def prerelease_local_scheme(version):
-    """
-    Return local scheme version unless building on master in CircleCI.
-
-    This function returns the local scheme version number
-    (e.g. 0.0.0.dev<N>+g<HASH>) unless building on CircleCI for a
-    pre-release in which case it ignores the hash and produces a
-    PEP440 compliant pre-release version number (e.g. 0.0.0.dev<N>).
-    """
-    from setuptools_scm.version import get_local_node_and_date
-
-    if os.getenv('CI_BRANCH') == 'master':
-        return ''
-    else:
-        return get_local_node_and_date(version)
-
-
 with open('README.md') as f:
     readme = f.read()
 
 # perform the install
 setup(
     name='django-s3-file-field',
-    use_scm_version={'local_scheme': prerelease_local_scheme},
-    setup_requires=['setuptools-scm'],
     description='A django widget library for securely uploading files directly to S3 (or MinIO).',
     long_description=readme,
     long_description_content_type='text/markdown',
