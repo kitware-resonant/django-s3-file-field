1c4ba6beb1fc9821b3843a34752b7ee508649248
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-02-23 11:46:48 -0500

    Use dict.get() default instead of if None

--- s3_file_field/views.py
+++ s3_file_field/views.py
@@ -87,13 +87,11 @@ def upload_initialize(request: Request) -> HttpResponseBase:
     # Maybe we need a different generate method/upload_to with a different signature?
     object_key = field.generate_filename(None, file_name)
 
-    content_type = upload_request.get('content_type', None)
-    if content_type is None:
-        content_type = mimetypes.guess_type(file_name)[0]
-
-    content_disposition = upload_request.get('content_disposition', None)
-    if content_disposition is None:
-        content_disposition = f'attachment; filename="{file_name}"'
+    content_type = upload_request.get('content_type', mimetypes.guess_type(file_name)[0])
+    content_disposition = upload_request.get(
+        'content_disposition',
+        f'attachment; filename="{file_name}"',
+    )
 
     initialization = _multipart.MultipartManager.from_storage(field.storage).initialize_upload(
         object_key,
