f634e23f936dcec4d5dba655249f921b0448e727
Brian Helba <brian.helba@kitware.com>
2021-10-19 12:45:39 -0400

    Remove default_app_config for new Django versions

--- s3_file_field/__init__.py
+++ s3_file_field/__init__.py
@@ -1,6 +1,11 @@
+import django
+
 # The documentation should always reference s3_file_field.S3FileField
 # and this cannot change without breaking the migrations of downstream
 # projects.
-from .fields import S3FileField  # noqa: F401
+from .fields import S3FileField
 
-default_app_config = 's3_file_field.apps.S3FileFieldConfig'
+if django.VERSION < (3, 2):
+    default_app_config = 's3_file_field.apps.S3FileFieldConfig'
+
+__all__ = ['S3FileField']
