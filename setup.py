d1b83607129bbf9606756e203131ff8e56d9626a
Brian Helba <brian.helba@kitware.com>
2022-01-23 21:41:26 -0500

    Add support for Python 3.10

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python',
     ],
     packages=find_packages(include=['s3_file_field']),
