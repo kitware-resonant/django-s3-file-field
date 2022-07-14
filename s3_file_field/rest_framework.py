95351876b28d75b75a3ca75cc8aa9bcde8f10689
Brian Helba <brian.helba@kitware.com>
2022-07-14 12:38:53 -0400

    Add type hints and harden validation of S3FileSerializerField

--- s3_file_field/rest_framework.py
+++ s3_file_field/rest_framework.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+from django.core.files import File
 from rest_framework.fields import FileField as FileSerializerField
 
 from s3_file_field.widgets import S3PlaceholderFile
@@ -8,14 +11,21 @@ class S3FileSerializerField(FileSerializerField):
         'invalid': 'Not a valid signed S3 upload. Ensure that the S3 upload flow is correct.',
     }
 
-    def to_internal_value(self, data):
+    def to_internal_value(self, data: Union[str, File]) -> str:  # type: ignore[override]
+        if isinstance(data, File):
+            # Although the parser may allow submission of an inline file, S3FF should refuse to
+            # accept it. We should assume that the server doesn't want to act as a proxy, so
+            # API callers shouldn't be rewarded for submitting inline files.
+            self.fail('invalid')
+
         # Check the signature and load an S3PlaceholderFile
         file_object = S3PlaceholderFile.from_field(data)
         if file_object is None:
             self.fail('invalid')
 
         # This checks validity of the file name and size
-        file_object = super().to_internal_value(file_object)
+        super().to_internal_value(file_object)
+        assert file_object.name
 
         # fields.S3FileField.save_form_data is not called by DRF, so the same behavior must be
         # implemented here
