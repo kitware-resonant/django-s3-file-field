5697e75616f707eb75ad8019360a7ee3636694a4
Zach Mullen <zach.mullen@kitware.com>
2025-08-12 20:21:05 -0400

    Make pydantic type return full `S3PlaceholderFile` instance

--- s3_file_field/ninja.py
+++ s3_file_field/ninja.py
@@ -1,15 +1,43 @@
-from typing import Annotated
+from typing import Annotated, Any
 
-from pydantic import BeforeValidator
+from pydantic import GetCoreSchemaHandler, GetJsonSchemaHandler
+from pydantic.json_schema import JsonSchemaValue
+from pydantic_core import core_schema
 
 from s3_file_field.widgets import S3PlaceholderFile
 
 
-def _convert_s3ff_value(value: str) -> str:
-    file_obj = S3PlaceholderFile.from_field(value)
-    if file_obj is None:
-        raise ValueError("Invalid S3 file field value")
-    return file_obj.name
+class _S3FileFieldValueTypeMetadata:
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls,
+        _source_type: Any,
+        _handler: GetCoreSchemaHandler,
+    ) -> core_schema.CoreSchema:
+        def validate_from_str(value: str) -> S3PlaceholderFile:
+            file_obj = S3PlaceholderFile.from_field(value)
+            if file_obj is None:
+                raise ValueError("Invalid S3 file field value")
+            return file_obj
+
+        from_str_schema = core_schema.no_info_before_validator_function(
+            validate_from_str,
+            schema=core_schema.is_instance_schema(S3PlaceholderFile),
+        )
+
+        return core_schema.json_or_python_schema(
+            json_schema=from_str_schema,
+            python_schema=from_str_schema,
+            serialization=core_schema.plain_serializer_function_ser_schema(
+                lambda instance: str(instance)
+            ),
+        )
+
+    @classmethod
+    def __get_pydantic_json_schema__(
+        cls, _core_schema: core_schema.CoreSchema, handler: GetJsonSchemaHandler
+    ) -> JsonSchemaValue:
+        return handler(core_schema.str_schema())
 
 
-S3FileFieldValue = Annotated[str, BeforeValidator(_convert_s3ff_value)]
+S3FileFieldValue = Annotated[S3PlaceholderFile, _S3FileFieldValueTypeMetadata]
